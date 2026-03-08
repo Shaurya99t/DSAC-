b00da504df7a0fd3e956dc5265adbd53270beaca
Shaurya99t <118307828+Shaurya99t@users.noreply.github.com>
2026-03-09 00:01:49 +0530

    Add files via upload

---
+++ Day_3/faran_cecius.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int c,f=0;
+    cout<<"Entet the celcius"<<endl;
+    cin>>c;
+    f =(c*9.0/5.0)+32;
+    cout<<f<<endl;
+    return 0;
+}
---
+++ Day_3/prime.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n;
+cout<<"Enter the number"<<endl;
+cin>>n;
+int i=2;
+while(i<n){
+    if (n%i==0){
+        cout<<"not a prime number"<<i<<endl;
+    }
+    else{
+        cout<<"prime number"<<i<<endl;
+
+    }
+    i++;
+}
+    return 0;
+}
